0212ffc9a54f74c7101669578ae05f27333c1b4f
lucaslcn <lucaslucian471@gmail.com>
2020-06-15 00:43:15 -0300

    commitTestes
    feitos alguns testes, 5 ou 6 testes

---
+++ ConfSofware/test/DAO/AlunoDAOTest.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Apoio.ConexaoBD;
+import Entidade.Aluno;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Order;
+
+/**
+ *
+ */
+public class AlunoDAOtest{
+    
+    public AlunoDAOtest() {
+    
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of salvarTeste method, of class AlunoDAO.
+     */
+    @Test
+    @Order(1)
+    public void testSalvarteste() {
+       System.out.println("salvar");
+        String alunoTest = "lukinha";
+
+        Object o = null;
+        Aluno aluno = new Aluno();
+        AlunoDAO instance = new AlunoDAO();
+        aluno.setId(100);
+        aluno.setNome(alunoTest);
+        
+        String expResult = null;
+        String result = instance.salvar(aluno);
+        assertEquals(expResult, result);
+    }
+    
+    
+    
+    //teste para assegurar que o novo insert está como default MATRICULADO
+    @Test
+    @Order(2)
+    public void testMatricula() throws SQLException {
+       System.out.println("salvar");
+        String alunoTest_situacao = "Matriculado";
+
+        System.out.println("testar status do aluno inserido no (1)");
+
+        String expResult = null;
+        
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "SELECT * FROM aluno "
+                    + "WHERE id = 100 AND status = 'Matriculado'";
+
+            ResultSet resultado = st.executeQuery(sql);
+
+            if (resultado.next()) {
+               Aluno aluno = new Aluno();
+               aluno.setId(resultado.getInt("id"));
+               aluno.setNome(resultado.getString("nome"));
+               aluno.setSituacao(resultado.getString("status"));
+               expResult = aluno.getSituacao();
+            } else {
+                System.out.println("oi");
+            }
+            
+        assertEquals(alunoTest_situacao, expResult);     
+    }
+    
+    
+    //teste cpf
+    @Test
+    @Order(3)
+    public void testCPF() throws SQLException {
+       System.out.println("salvar");
+        String cpf = null;
+
+        System.out.println("testar cpf do aluno inserido no (1)");
+
+        String expResult = "03086447001";
+        
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "SELECT * FROM aluno "
+                    + "WHERE id = 100";
+
+            ResultSet resultado = st.executeQuery(sql);
+
+            if (resultado.next()) {
+               Aluno aluno = new Aluno();
+               aluno.setId(resultado.getInt("id"));
+               aluno.setNome(resultado.getString("nome"));
+               aluno.setSituacao(resultado.getString("status"));
+               aluno.setCpf(resultado.getString("cpf"));
+               cpf = aluno.getCpf();
+            } else {
+                System.out.println("oi");
+            }
+            
+        assertEquals(expResult,cpf);     
+    }
+    
+    
+}
--- ConfSofware/test/DAO/DisciplinaDAOTest.java
+++ ConfSofware/test/DAO/DisciplinaDAOTest.java
@@ -5,8 +5,12 @@
  */
 package DAO;
 
+import Apoio.ConexaoBD;
 import Entidade.Disciplina;
 import Entidade.Turma;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import javax.swing.JTable;
 import org.junit.After;
@@ -15,6 +19,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Order;
 
 /**
@@ -22,8 +27,9 @@ import org.junit.jupiter.api.Order;
  * @author elias
  */
 public class DisciplinaDAOTest {
-    
+        
     public DisciplinaDAOTest() {
+    
     }
     
     @BeforeClass
@@ -49,15 +55,79 @@ public class DisciplinaDAOTest {
     @Order(1)
     public void testSalvarTeste() {
        System.out.println("salvar");
+        String disciplinaTest = "Extreme programming";
+
         Object o = null;
         Disciplina disciplina = new Disciplina();
         DisciplinaDAO instance = new DisciplinaDAO();
         disciplina.setId(100);
-        disciplina.setNome("Extreme Programming");
+        disciplina.setNome(disciplinaTest);
         disciplina.setCarga(60);
         
         String expResult = null;
         String result = instance.salvarTeste(disciplina);
         assertEquals(expResult, result);
     }
-}
+    
+    //testa selecionar a disciplina do id anterior
+    
+    @Test
+    @Order(2)
+    public void testSelect() throws SQLException
+    {
+        System.out.println("testar nome da disciplina inserida no teste anterior (1)");
+        String disciplinaTest_name = "Extreme programming";
+        int disciplinaTest_carga_horaria = 60;
+
+        String expResult = null;
+        
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "SELECT * FROM disciplina "
+                    + "WHERE id = 100";
+
+            ResultSet resultado = st.executeQuery(sql);
+
+            if (resultado.next()) {
+               Disciplina disciplina = new Disciplina();
+               disciplina.setId(resultado.getInt("id"));
+               disciplina.setNome(resultado.getString("nome"));
+               disciplina.setCarga(resultado.getInt("carga_horaria"));
+               expResult = disciplina.getNome();
+            } else {
+                System.out.println("oi");
+            }
+        assertEquals(disciplinaTest_name, expResult);     
+    }
+
+    @Test
+    @Order(3)
+    public void testCargaHoraria() throws SQLException
+    {
+        System.out.println("testar carga horária da disciplina inserida no teste (1)");
+        String disciplinaTest = "Extreme programming";
+        int disciplinaTest_carga_horaria = 60;
+
+        int expResult = 0;
+        
+            Statement st = ConexaoBD.getInstance().getConnection().createStatement();
+
+            String sql = "SELECT * FROM disciplina "
+                    + "WHERE id = 100";
+
+            ResultSet resultado = st.executeQuery(sql);
+
+            if (resultado.next()) {
+               Disciplina disciplina = new Disciplina();
+               disciplina.setId(resultado.getInt("id"));
+               disciplina.setNome(resultado.getString("nome"));
+               disciplina.setCarga(resultado.getInt("carga_horaria"));
+               expResult = disciplina.getCarga();
+            } else {
+                System.out.println("oi");
+            }
+        assertEquals(disciplinaTest_carga_horaria, expResult);     
+    }
+    
+    
+    }
